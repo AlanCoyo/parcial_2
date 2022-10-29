fc636ae3420ec2d0117d9f75edb215a8a4c52ff2
AlanCoyo <0238099@up.edu.mx>
2022-10-29 12:20:05 -0500

    Correcciones y nuevos endpoints

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express =  require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const {router} = require("./routes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const {router} = require ('./routes')
 
-const app=express();
-const apiPort=2508;
+const app = express();
+const apiPort = process.env.Api_PORT || 3003;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-app.use(bodyParser.json());
-app.use("/",router);
+app.use(bodyParser.json())
+ 
+app.use('/',router);
 
-app.listen(apiPort,()=>{
-    console.log(`Servidor iniciado en el puerto ${apiPort}`)
-})
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`)); 
